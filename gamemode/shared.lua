ee3bb7365d7bbc8e17ec927ef8894bdc58ea3387
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-10 15:43:11 -0400

    added base class?

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -5,20 +5,6 @@ GM.Website = "NA"
 
 DeriveGamemode("sandbox")
 
-AddCSLuaFile("player_class/animal.lua")
-include("player_class/animal.lua")
-
-function GM:PlayerSpawn(ply)
-    ply:Say('huh? world')
-    player_manager.SetPlayerClass( ply, "player_custom" )
-    -- player_manager.OnPlayerSpawn(ply)
-    -- ply:SetGravity(0)
-    -- ply:SetMaxHealth(104)
-    -- ply:SetRunSpeed(500)
-    -- ply:SetWalkSpeed(30)
-    -- ply:Give("weapon_physgun")
-end
-
 function GM:Initialize()
     --dostudff
 end
