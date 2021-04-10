1289ea336372bc09768eaa52a636bc788196230c
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-10 15:49:19 -0400

    added base class?

--- gamemode/player_class/animal.lua
+++ gamemode/player_class/animal.lua
@@ -1,6 +1,24 @@
+Account = { balance=0,
+            withdraw = function (self, v)
+                            self.balance = self.balance - v
+                        end,
+            neat = 13
+            }
+
+function Account:deposit (v)
+    self.balance = self.balance + v
+end
+
+function Account:new (o)
+    o = o or {}   -- create object if user does not provide one
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
 Animal = {
-    walkSpeed = 100
+    walkSpeed = 100,
     runSpeed = 1000
 }
-
-print(Animal.walkSpeed .. Animal.runSpeed)
+a = Animal
+print(a.walkSpeed .. b.runSpeed)
