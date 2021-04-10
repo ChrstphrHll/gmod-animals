3273279418f5ecd8501b72a8d3c9d9e2d542e1da
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-10 15:25:57 -0400

    added base class?

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -5,15 +5,7 @@ AddCSLuaFile("player_class/animal.lua")
 include("shared.lua")
 include("player_class/animal.lua")
 
-function GM:PlayerSpawn(ply)
-    ply:Say('huh? world')
-    player_manager.SetPlayerClass( ply, "player_custom" )
-    -- ply:SetGravity(0)
-    -- ply:SetMaxHealth(104)
-    -- ply:SetRunSpeed(500)
-    -- ply:SetWalkSpeed(30)
-    -- ply:Give("weapon_physgun")
-end
+
 
 function GM:OnNPC(npc, attacker, inflictor)
     
--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -8,6 +8,15 @@ DeriveGamemode("sandbox")
 AddCSLuaFile("player_class/animal.lua")
 include("player_class/animal.lua")
 
+function GM:PlayerSpawn(ply)
+    ply:Say('huh? world')
+    player_manager.SetPlayerClass( ply, "player_custom" )
+    -- ply:SetGravity(0)
+    -- ply:SetMaxHealth(104)
+    -- ply:SetRunSpeed(500)
+    -- ply:SetWalkSpeed(30)
+    -- ply:Give("weapon_physgun")
+end
 
 function GM:Initialize()
     --dostudff
