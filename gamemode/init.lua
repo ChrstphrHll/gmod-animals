e9dc2792d6ed9f8077d93a425afc9a42ee963d12
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-17 14:36:28 -0400

    added custom hud shell

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,6 +4,19 @@ AddCSLuaFile("testhud.lua")
 
 include("shared.lua")
 
+local PANEL = {}
+
+function PANEL:Init()
+    self:SetSize( 100, 100 )
+    self:Center()
+end
+
+function PANEL:Paint( w, h )
+    draw.RoundedBox( 0, 0, 0, w, h, color_black )
+end
+
+vgui.Register( "MyFirstPanel", PANEL, "Panel" )
+
 function GM:PlayerSpawn(ply)
     ply:Say('huh? world')
     ply:SetGravity(0.8)
