ee8daf500903a394e1850b0b0011219d966fd591
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-22 15:36:08 -0400

    fast powerup

---
+++ entities/entities/become_fast/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Activities"
+
+ENT.Model = "models/props_lab/reciever_cart.mdl"
+
+ENT.PrintName = "Speed Power Up"
+ENT.Author = "Christopher"
+ENT.Purpose = "Changes player speed"
+ENT.Instructions = "Place and use"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
