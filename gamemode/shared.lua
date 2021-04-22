41140121fc1f93c8979e2cc5dadfbbefbac47029
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-22 16:53:55 -0400

    Spawn system based on a players currentAnimal network string

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -7,6 +7,12 @@ DeriveGamemode("sandbox")
 AddCSLuaFile("player_class/Animal.lua")
 include("player_class/Animal.lua")
 
+AddCSLuaFile("player_class/FelineAnimal.lua")
+include("player_class/FelineAnimal.lua")
+
+AddCSLuaFile("player_class/FlyingAnimal.lua")
+include("player_class/FlyingAnimal.lua")
+
 function GM:Initialize()
 
 end
