2a778cc20332b25e1550e892a5a841c44ffd3362
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-22 19:23:30 -0400

    Spawn system based on a players currentAnimal network string

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -3,7 +3,9 @@ include("testhud.lua")
 
 net.Receive("f4menu", function()
     if (!Frame) then
+        local selected = nil
         local ply = LocalPlayer()
+        local plyCurrentKarma = ply:GetNWInt('karma')
         local Frame = vgui.Create( "DFrame" )
         Frame:SetTitle( "Karma Store" )
         Frame:SetSize( 300,300 )
@@ -12,7 +14,24 @@ net.Receive("f4menu", function()
         Frame.Paint = function( self, w, h ) -- 'function Frame:Paint( w, h )' works too
             draw.RoundedBox( 0, 0, 0, w, h, Color( 231, 76, 60, 150 ) ) -- Draw a red box instead of the frame
         end
-                
+
+        local DComboBox = vgui.Create( "DComboBox", Frame )
+        DComboBox:SetPos( 5, 30 )
+        DComboBox:SetSize( 100, 20 )
+        DComboBox:SetValue( "Availible Animals" )
+        for k in pairs(ImplementedAnimals) do 
+            AnimalObject = ImplementedAnimals[k]
+            print(AnimalObject.DisplayName .. ' ' .. AnimalObject.KarmaCost)
+            if (plyCurrentKarma >= AnimalObject.KarmaCost) then
+                DComboBox:AddChoice( AnimalObject.DisplayName ) 
+            end
+        end
+
+        DComboBox.OnSelect = function( self, index, value )
+            print( value .." was selected at index " .. index )
+            selected = value
+        end
+
         local Button = vgui.Create("DButton", Frame)
         --Button:SetText( "Click me I'm pretty!" )
         Button:SetText( ply:Nick() )
@@ -24,24 +43,10 @@ net.Receive("f4menu", function()
         end
         Button.DoClick = function()
             print( "I was clicked!" )
-        end
-
-        local DComboBox = vgui.Create( "DComboBox", Frame )
-        DComboBox:SetPos( 5, 30 )
-        DComboBox:SetSize( 100, 20 )
-        DComboBox:SetValue( "Availible Animals" )
-        print('Player karma ' .. ply:GetNWInt('karma'))
-        print(ImplementedAnimals)
-        for k in pairs(ImplementedAnimals) do 
-            AnimalObject = ImplementedAnimals[k]
-            print(AnimalObject.DisplayName .. ' ' .. AnimalObject.KarmaCost)
-            if (ply:GetNWInt('karma') >= AnimalObject.KarmaCost) then
-                DComboBox:AddChoice( AnimalObject.DisplayName ) 
+            if (selected) then
+                ply:SetNWInt('karma', plyCurrentKarma - ImplementedAnimals[selected].KarmaCost)
+                Frame:Close()
             end
-        end
-
-        DComboBox.OnSelect = function( self, index, value )
-            print( value .." was selected at index " .. index )
         end
 
         Frame:ShowCloseButton(false)
