d680c525ff26517b594f6b967624fd484a28fc65
Alan Bi <alan.bi326@gmail.com>
2021-04-22 23:49:23 -0700

    Add end life activity

---
+++ entities/entities/end_life/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.Category = "Activities"
+
+ENT.Model = "models/props_lab/reciever_cart.mdl"
+
+ENT.PrintName = "End Life Power Up"
+ENT.Author = "Christopher"
+ENT.Purpose = "End current life to move on to next"
+ENT.Instructions = "Place and use"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
