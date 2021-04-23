5cc4257752e04c1de7d7c802eea7a719c9eaec62
Alan Bi <alan.bi326@gmail.com>
2021-04-22 22:22:45 -0700

    Create gain karma activity

---
+++ entities/entities/gain_karma/init.lua
@@ -0,0 +1,45 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include ("shared.lua")
+
+function ENT:Initialize()
+    self:SetModel(self.Model)
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_VPHYSICS)
+
+    self:SetUseType(SIMPLE_USE)
+
+    local phys = self:GetPhysicsObject()
+
+    if (IsValid(phys)) then
+        phys:Wake()
+    end
+
+end
+
+function ENT:SpawnFunction(ply, tr, ClassName)
+    if (!tr.Hit) then return end
+
+    local SpawnPos = ply:GetShootPos() + ply:GetForward() * 80
+
+    local ent = ents.Create(ClassName)
+    ent:SetPos(SpawnPos)
+    ent:Spawn()
+    ent:Activate()
+
+    return ent
+end
+
+function ENT:Use(activator, caller)
+    -- Whenever a player uses the entity
+    activator:SetWalkSpeed(1000)
+    print("Gaining 50 karma...")
+    activator.SetNWInt("karma", activator.GetNWInt("karma") + 50)
+
+end
+
+function ENT:Think()
+    -- Called every tick
+end
