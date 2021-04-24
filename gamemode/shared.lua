78dfc0ef2682b0676f961035c8534de20cb1e28a
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-24 12:02:36 -0400

    added clawed animal

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,11 +4,14 @@ GM.Email = "N?A"
 GM.Website = "NA"
 
 DeriveGamemode("sandbox")
-AddCSLuaFile("player_class/Animal.lua")
-include("player_class/Animal.lua")
+-- AddCSLuaFile("player_class/Animal.lua")
+-- include("player_class/Animal.lua")
 
-AddCSLuaFile("player_class/FelineAnimal.lua")
-include("player_class/FelineAnimal.lua")
+-- AddCSLuaFile("player_class/FelineAnimal.lua")
+-- include("player_class/FelineAnimal.lua")
+
+AddCSLuaFile("player_class/ClawedAnimal.lua")
+include("player_class/ClawedAnimal.lua")
 
 -- TODO: an error exists rn where only the flying animals show up but not the feline animals
 -- commenting out these two lines fixes it, so i'm guessing some conflict/overriding issue is occurring
