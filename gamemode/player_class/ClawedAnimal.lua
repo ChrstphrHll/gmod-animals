460977e361092657e578065342c6b29ed89bc257
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-25 16:16:00 -0400

    changed some news to deriveds

--- gamemode/player_class/ClawedAnimal.lua
+++ gamemode/player_class/ClawedAnimal.lua
@@ -15,17 +15,6 @@ Hawk = ClawedFlyer:new({
     FlySpeed = '500'
 })
 
-function lookatmetaof(o)
-    for key,value in pairs(o) do
-        print(key, value)
-        if key == '__index' then
-            lookatmetaof(value)
-        end
-    end
-end
-
-lookatmetaof(getmetatable(Hawk))
-
 Hawk.test()
 -- print(ClawedFlyer.loadout)
 -- print(ClawedFlyer.specialAbility)
