4b6d89e0ce8ddeedf7f6ace64ee8b34bb87b38cf
Christopher Hall II <christopher.s.hall@duke.edu>
2021-04-25 22:21:26 -0400

    retooling clawed modifier

--- gamemode/player_class/ClawedAnimal.lua
+++ gamemode/player_class/ClawedAnimal.lua
@@ -1,20 +1,28 @@
 include("FelineAnimal.lua")
 include("Hybrid.lua")
 
-Clawed = {}
-function Clawed:loadout (ply)
-    ply:Give("weapon_crowbar")
+function makeClawed(Animal)
+    Animal.loadout = function(ply) ply:Give("weapon_crowbar") end
 end
 
---ClawedFlyer = Clawed:derived(FlyingAnimal:derived({DisplayName = 'ClawedFlyer'}))
-ClawedFlyer = createClass(FlyingAnimal, Clawed)
+ClawedFlyer = FlyingAnimal:derived({DisplayName = 'ClawedFlyer'}))
+makeClawed(ClawedFlyer)
+
+ClawedFeline = FelineAnimal:derived({DisplayName = 'ClawedFeline'})
 
 Hawk = ClawedFlyer:new({
     DisplayName = 'Hawk',
     Playable = true,
     KarmaCost = 74,
-    FlySpeed = '500'
+    FlySpeed = 500
 })
 
-print(ClawedFlyer.loadout)
-print(ClawedFlyer.specialAbility)
+Puma = ClawedFeline:new({
+    DisplayName = 'Puma',
+    Playable = true,
+    KarmaCost = 500,
+    Pounce = 700
+})
+
+-- print(ClawedFlyer.loadout)
+-- print(ClawedFlyer.specialAbility)
